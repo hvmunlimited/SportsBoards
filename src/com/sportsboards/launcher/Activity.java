411d8bebc67f165a2563270aeb38680549cbd014
Nathan King <nathanking007@gmail.com>
2011-01-17 12:24:16 -0600

    added zoom

--- src/com/sportsboards/launcher/Activity.java
+++ src/com/sportsboards/launcher/Activity.java
@@ -1,14 +1,15 @@
 package com.sportsboards.launcher;
 
 import org.anddev.andengine.ui.activity.BaseGameActivity;
+
 import com.sportsboards.R;
 import com.sportsboards.SoccerBoard;
-
+import com.sportsboards.FootBallBoard;
 
 enum Activity{
 
 	SOCCER(SoccerBoard.class, R.string.soccer_string);
-	
+	//FOOTBALL(FootBallBoard.class R.string.football_string);
 	
 
 	public final Class<? extends BaseGameActivity> Class;
